d9a427ee9bd2e264a0a2a1381a57677e1e902433
Aaron Forshaw <aaron@janda.org.uk>
2021-09-09 09:27:03 +0100

    mc-9289 Enumeration and summary metadata

--- src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/database/mysql/MySqlDatabaseDataModelImporterProviderService.groovy
+++ src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/database/mysql/MySqlDatabaseDataModelImporterProviderService.groovy
@@ -17,19 +17,27 @@
  */
 package uk.ac.ox.softeng.maurodatamapper.plugins.database.mysql
 
+import grails.util.Pair
 import uk.ac.ox.softeng.maurodatamapper.api.exception.ApiException
 import uk.ac.ox.softeng.maurodatamapper.datamodel.DataModel
+import uk.ac.ox.softeng.maurodatamapper.datamodel.facet.SummaryMetadata
 import uk.ac.ox.softeng.maurodatamapper.datamodel.item.DataClass
 import uk.ac.ox.softeng.maurodatamapper.datamodel.item.DataElement
 import uk.ac.ox.softeng.maurodatamapper.datamodel.item.datatype.DataType
+import uk.ac.ox.softeng.maurodatamapper.datamodel.item.datatype.EnumerationType
+import uk.ac.ox.softeng.maurodatamapper.datamodel.item.datatype.enumeration.EnumerationValue
 import uk.ac.ox.softeng.maurodatamapper.plugins.database.AbstractDatabaseDataModelImporterProviderService
 import uk.ac.ox.softeng.maurodatamapper.plugins.database.RemoteDatabaseDataModelImporterProviderService
+import uk.ac.ox.softeng.maurodatamapper.plugins.database.summarymetadata.AbstractIntervalHelper
 
 import groovy.json.JsonOutput
+import uk.ac.ox.softeng.maurodatamapper.plugins.database.summarymetadata.SummaryMetadataHelper
+import uk.ac.ox.softeng.maurodatamapper.security.User
 
 import java.sql.Connection
 import java.sql.PreparedStatement
 import java.sql.SQLException
+import java.time.format.DateTimeFormatter
 
 // @CompileStatic
 class MySqlDatabaseDataModelImporterProviderService
@@ -165,6 +173,153 @@ class MySqlDatabaseDataModelImporterProviderService
             columnElement.addToMetadata(namespace, "foreign_key_columns", row.reference_column_name as String, dataModel.createdBy)
         }
 
+    }
+
+   /**
+    * The MySQL identifier quote character is the backtick (`). See
+    * https://dev.mysql.com/doc/refman/8.0/en/identifiers.html
+    */
+    @Override
+    String escapeIdentifier(String identifier) {
+        "`${identifier}`"
+    }
+
+    @Override
+    boolean isColumnPossibleEnumeration(DataType dataType) {
+        dataType.domainType == 'PrimitiveType' && ["CHAR", "VARCHAR"].contains(dataType.label.toUpperCase())
+    }
+
+    /**
+     * Override the base method because for MySQL we don't have the schemaClass
+     * @param user
+     * @param maxEnumerations
+     * @param dataModel
+     * @param connection
+     */
+    @Override
+    void updateDataModelWithEnumerations(User user, int maxEnumerations, DataModel dataModel, Connection connection) {
+        dataModel.childDataClasses.each { DataClass tableClass ->
+            tableClass.dataElements.each { DataElement de ->
+                DataType primitiveType = de.dataType
+                if (isColumnPossibleEnumeration(primitiveType)) {
+                    int countDistinct = getCountDistinctColumnValues(connection, de.label, tableClass.label)
+                    if (countDistinct > 0 && countDistinct <= maxEnumerations) {
+                        EnumerationType enumerationType = enumerationTypeService.findOrCreateDataTypeForDataModel(dataModel, de.label, de.label, user)
+
+                        final List<Map<String, Object>> results = getDistinctColumnValues(connection, de.label, tableClass.label)
+
+                        replacePrimitiveTypeWithEnumerationType(dataModel, de, primitiveType, enumerationType, results)
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Compute a value distribution for relevant columns and store as summary metadata.
+     * @param user
+     * @param dataModel
+     * @param connection
+     */
+    void updateDataModelWithSummaryMetadata(User user, DataModel dataModel, Connection connection) {
+        dataModel.childDataClasses.each { DataClass tableClass ->
+            tableClass.dataElements.each { DataElement de ->
+                DataType dt = de.dataType
+                if (isColumnForDateSummary(dt) || isColumnForDecimalSummary(dt) || isColumnForIntegerSummary(dt)) {
+                    Pair minMax = getMinMaxColumnValues(connection, de.label, tableClass.label)
+
+                    //aValue is the MIN, bValue is the MAX. If they are not null then calculate the range etc...
+                    if (!(minMax.aValue == null) && !(minMax.bValue == null)) {
+                        AbstractIntervalHelper intervalHelper = getIntervalHelper(dt, minMax)
+
+                        Map<String, Integer> valueDistribution = getColumnRangeDistribution(connection, dt, intervalHelper, de.label, tableClass.label)
+                        if (valueDistribution) {
+                            SummaryMetadata summaryMetadata = SummaryMetadataHelper.createSummaryMetadataFromMap(user, de.label, 'Value Distribution', valueDistribution)
+                            de.addToSummaryMetadata(summaryMetadata);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    boolean isColumnForDateSummary(DataType dataType) {
+        dataType.domainType == 'PrimitiveType' && ["DATE", "DATETIME", "TIMESTAMP"].contains(dataType.label.toUpperCase())
+    }
+
+    @Override
+    boolean isColumnForDecimalSummary(DataType dataType) {
+        dataType.domainType == 'PrimitiveType' && ["DECIMAL", "NUMERIC"].contains(dataType.label.toUpperCase())
+    }
+
+    @Override
+    boolean isColumnForIntegerSummary(DataType dataType) {
+        dataType.domainType == 'PrimitiveType' && ["INTEGER", "INT", "SMALLINT", "TINYINT", "MEDIUMINT", "BIGINT"].contains(dataType.label.toUpperCase())
+    }
+
+    @Override
+    String columnRangeDistributionQueryString(DataType dataType, AbstractIntervalHelper intervalHelper, String columnName, String tableName, String schemaName) {
+        List<String> selects = intervalHelper.intervals.collect {
+            "SELECT '${it.key}' AS interval_label, ${formatDataType(dataType, it.value.aValue)} AS interval_start, ${formatDataType(dataType, it.value.bValue)} AS interval_end "
+        }
+
+        rangeDistributionQueryString(selects, columnName, tableName)
+    }
+
+    /**
+     * If dataType represents a date then return a string which uses the Oracle TO_DATE function to
+     * convert the value to a date, otherwise just return the value as a string
+     *
+     * @param dataType
+     * @param value
+     * @return fragment of query like STR_TO_DATE('2020-08-15 23:18:00', '%Y-%m-%d %h:%i:%s')
+     * or a string
+     */
+    String formatDataType(DataType dataType, Object value) {
+        if (isColumnForDateSummary(dataType)){
+            "STR_TO_DATE('${DateTimeFormatter.ISO_LOCAL_DATE.format(value)} ${DateTimeFormatter.ISO_LOCAL_TIME.format(value)}', '%Y-%m-%d %H:%i:%s')"
+        } else {
+            "${value}"
+        }
+    }
+
+    /**
+     * Returns a String that looks, for example, like this:
+     * WITH mdm_interval AS (
+     *   SELECT '0 - 100' AS interval_label, 0 AS interval_start, 100 AS interval_end
+     *   UNION
+     *   SELECT '100 - 200' AS interval_label, 100 AS interval_start, 200 AS interval_end
+     * )
+     * SELECT interval_label, COUNT(`my_column`) AS interval_count
+     * FROM mdm_interval
+     * LEFT JOIN
+     * `my_table` ON `my_table`.`my_column` >= mdm_interval.interval_start
+     * AND `my_table` ON `my_table`.`my_column`< mdm_interval.interval_end
+     * GROUP BY interval_label, interval_start
+     * ORDER BY interval_start ASC;
+     *
+     * @param tableName
+     * @param columnName
+     * @param selects
+     * @return Query string for intervals, using Oracle SQL
+     */
+    private String rangeDistributionQueryString(List<String> selects, String columnName, String tableName) {
+        String intervals = selects.join(" UNION ")
+
+        String sql = "WITH mdm_interval AS (${intervals})" +
+                """
+        SELECT interval_label, COUNT(${escapeIdentifier(columnName)}) AS interval_count
+        FROM mdm_interval
+        LEFT JOIN
+        ${escapeIdentifier(tableName)} 
+        ON ${escapeIdentifier(tableName)}.${escapeIdentifier(columnName)} >= mdm_interval.interval_start 
+        AND ${escapeIdentifier(tableName)}.${escapeIdentifier(columnName)} < mdm_interval.interval_end
+        GROUP BY interval_label, interval_start
+        ORDER BY interval_start ASC
+        """
+
+        sql.stripIndent()
     }
 
     List<Map<String, Object>> executePreparedStatement(DataModel dataModel, Connection connection,
